ef69c388fcb2f2c97aeb3f7eb43c15f4de95dda7
Mairhofer <01MaiPat>
2018-04-16 09:04:32 +0200

    Finale Version der Klasse Berechnung
    Signed-off-by: Mairhofer <01MaiPat>

--- Widerstandsberechner/Widerstandsberechner/Program.cs
+++ Widerstandsberechner/Widerstandsberechner/Program.cs
@@ -11,4 +11,42 @@ namespace Widerstandsberechner
         {
         }
     }
+    class Berechnung
+    {
+        private double r1;
+        private double r2;
+        private double rend;
+        private bool sorp;
+
+        public void setr1(double wert1)
+        {
+            r1 = wert1;
+        }
+        public void setr2(double wert2)
+        {
+            r2 = wert2;
+        }
+        private double berechnen()
+        {
+            double endwert = 0;
+            if (sorp == true)
+            {
+                endwert = r1 + r2;
+            }
+            else if (sorp == false)
+            {
+                endwert = (r1 * r2) / (r1 + r2);
+            }
+            return endwert;
+        }
+        public void setsorp(bool set)
+        {
+            sorp = set;
+        }
+        public double getrend()
+        {
+            rend = berechnen();
+            return rend;
+        }
+    }
 }
